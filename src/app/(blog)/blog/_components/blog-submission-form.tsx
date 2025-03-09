e50424a2536aaa20910e633887c0f8192c958fe3
codewithtabish <codewithtabish16@gmail.com>
2025-03-08 23:03:00 -1200

    setting views schema

---
+++ src/app/(blog)/blog/_components/blog-submission-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogSubmissionForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BlogSubmissionForm
---
+++ src/app/(blog)/blog/create/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BlogSubmissionForm from '../_components/blog-submission-form'
+
+const CreateBlogPage = () => {
+  return (
+    <div>
+
+        <BlogSubmissionForm/>
+      
+    </div>
+  )
+}
+
+export default CreateBlogPage
